26778e1ae0502f5a662415c018e6734492d45fd9
masayuki_ide <hoop.it.up.m.ide@gmail.com>
2015-09-23 06:30:08 +0000

    followings-followers comp

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,12 @@ Rails.application.routes.draw do
   resources :microposts
   
   resources :relationships, only: [:create, :destroy]
+  
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
+  
+
 end
